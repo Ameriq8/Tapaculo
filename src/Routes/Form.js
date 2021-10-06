296ecca1a87522fa32aed1e76198ddb86cf8aea5
Ameriq8 <wwwwwwssssss91@gmail.com>
2021-10-06 14:26:06 +0300

    Add new routers (add-response, get-form), New changes in this routers (create form, edit form)

--- src/Routes/Form.js
+++ src/Routes/Form.js
@@ -1,23 +1,44 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const Form = require("../Database/Schema/Form");
 const CheckAuth = require("../Middleware/CheckAuth");
+const GenKey = require("../Utils/GenKey");
 
 router.get("/", CheckAuth, async (req, res) => {
-  let forms = await Form.findOne({userID: req.user});
+  let forms = await Form.findOne({ userID: req.user });
   res.status(200).json(forms);
 });
 
-router.post("/new-form", CheckAuth, async (req, res) => {
-  let {questions} = req.body;
+router.get("/:id", CheckAuth, async (req, res) => {
+  let form = await Form.findOne({ _id: req.params.id });
+  if (!form) return res.status(400).json({ msg: "No form found!!" })
+  res.status(200).json(form);
+});
 
-  if (!questions.length) return res.status(400).json({ msg: "Questions field is empty" });
+router.post("/new", CheckAuth, async (req, res) => {
+  let { description, questions } = req.body;
+
+  if (!description.length)
+    return res.status(400).json({ msg: "No description found!!" });
+  if (!questions.length)
+    return res.status(400).json({ msg: "No questions found!!" });
+
+  let newQuestionsArray = [];
+
+  for (let q in questions) {
+    newQuestionsArray.push({
+      questionID: GenKey(),
+      questionRequired: q.questionRequired,
+      questionType: q.questionType,
+    })
+  }
 
   let form = {
     userID: req.user,
+    description: description,
     status: true,
     createAt: new Date().getTime(),
-    questions
-  }
+    questions: newQuestionsArray,
+  };
 
   let newForm = new Form(form);
   newForm.save({}, (err) => {
@@ -25,26 +46,59 @@ router.post("/new-form", CheckAuth, async (req, res) => {
       res.status(400).json({ msg: err.message });
     }
 
-    res.status(200).json({ msg: "Successfully create the form and save it in the database" });
+    res.status(200).json({
+      msg: "Successfully create the form and save it in the database",
+    });
   });
 });
 
-router.put("/update-form/:formID", CheckAuth, async (req, res) => {
+router.put("/:formID", CheckAuth, async (req, res) => {
   let { formID } = req.params;
-  let { questions } = req.body
+  let { data } = req.body;
 
-  if (!questions.length) return res.status(400).json({ msg: "Questions field is empty" });
+  if (!data.length)
+    return res.status(400).json({ msg: "No data found!!" });
 
-  let form = await Form.findOneAndUpdate({ _id: formID }, {questions: questions}, {new: true});
+  let form = await Form.findOneAndUpdate(
+    { _id: formID },
+    {
+      description: data.description,
+      questions: data.questions
+    },
+    { new: true }
+  );
 
-  res.status(200).json({ msg: "Successfully create the form and save it in the database" });
+  res
+    .status(200)
+    .json({ msg: "Successfully create the form and save it in the database" });
 });
 
-router.delete("/delete-form/:formID", CheckAuth, async (req, res) => {
+router.delete("/:formID", CheckAuth, async (req, res) => {
   const form = await Form.findOne({ _id: req.params.formID });
   if (!form) return res.status(400).json({ msg: "No form found!!" });
   const deletedForm = await Form.findByIdAndDelete({ _id: form._id });
-  res.status(200).json({ msg: "Successfully deleted the form from the database" });
-})
+  res
+    .status(200)
+    .json({ msg: "Successfully deleted the form from the database" });
+});
+
+router.post("/add-response/:formID", CheckAuth, async (req, res) => {
+  let form = await Form.findOne({ _id: req.params.formID });
+  if (!form) return res.status(400).json({ msg: "No form found!!" });
+
+  let response = {
+    userID: req.user,
+    responseID: GenKey(),
+    response: req.body.response
+  }
+
+  form.responses.push(response);
+  form.markModified("response");
+  form.save();
+
+  res
+    .status(200)
+    .json({ msg: "Successfully add the response to database" });
+});
 
 module.exports = router;
